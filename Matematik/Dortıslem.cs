201c850ef70ba34647c4d2dc6dcc62c1af13ab16
merve çalışır <m-r-v-c-96@hotmail.com>
2021-01-21 18:20:40 +0300

    16 Ocak Ders Sonu

---
+++ Matematik/Dortıslem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class Dortıslem
+    {
+        public void Topla(int sayı1, int sayı2)
+        {
+            int toplam = sayı1 + sayı2;
+            Console.WriteLine("Sonuç : " + toplam);
+
+        }
+
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dortıslem dortıslem = new Dortıslem();
+            dortıslem.Topla(5, 6);
+
+            dortıslem.Topla(6, 9);
+
+
+
+        }
+    }
+}
